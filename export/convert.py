e5a99030c61ea5e4c8f58648d1906308a105e3dc
unknown <socpite@gmail.com>
2023-06-18 17:07:19 +0700

    Gif

---
+++ export/convert.py
@@ -0,0 +1,16 @@
+import glob
+from pathlib import Path
+import imageio
+FOLDER_NAME = "images\\board2_sol2"
+
+def srt(a: str):
+    return int(Path(a).stem)
+
+image_names = glob.glob(f"{FOLDER_NAME}\\*.png")
+image_names = sorted(image_names, key=srt)
+
+images = []
+for name in image_names:
+    images.append(imageio.imread(name))
+
+imageio.mimsave(f"{FOLDER_NAME}\\result.gif", images)
